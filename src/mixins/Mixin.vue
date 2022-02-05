2e3ad7b084972f71b0e499a9aeeeff9a919dcdb4
manami-sato <manamisato2001@gmail.com>
2022-02-06 02:31:23 +0900

    commit

--- src/mixins/Mixin.vue
+++ src/mixins/Mixin.vue
@@ -3,8 +3,23 @@ export default {
   data() {
     return {
       path: "https://click.ecc.ac.jp/ecc/msatou/raict_app/",
-      // routerPath: "/",
-      routerPath: "/ecc/msatou/raict_app/",
+      imgPath: "https://manami-sato.github.io/practice-site_1/src/assets/img/",
+      // name: "顔芸",
+      name: "サンプル",
+      // enName:"Kaogaming",
+      enName: "Sample",
+      URL: {
+        youtube: "https://www.youtube.com/channel/UCYxLnfa6hsEgg8rt4l96MSQ",
+        kirinuki: "https://www.youtube.com/channel/UC_0OHYcCLakDwDBHiwGp-pw",
+        skilltown: "https://skilltown.jp/items/detail/2346",
+        twitter: "https://twitter.com/kaogei_jp",
+      },
+      dummyURL: {
+        youtube: "#",
+        kirinuki: "#",
+        skilltown: "#",
+        twitter: "#",
+      },
     };
   },
 };
